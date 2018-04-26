9037e1e70c2117e1e8fc2ff8058dd04dfd6b21c9
Matt Perron <Perron.Matt@gmail.com>
2018-04-25 20:02:55 -0400

    fixed tpch_perfect sizes

--- tpch_perfect_sizes.py
+++ tpch_perfect_sizes.py
@@ -2,7 +2,7 @@ import os
 import struct
 
 def main():
-    NUM_QUERIES = 22 
+    NUM_QUERIES = 113 
     NUM_COMBS = 150000
     LONG_SIZE = 8
     count = 0
@@ -23,14 +23,12 @@ def main():
     with open('tpch_sizes', 'wb') as tpch_sizes:
         for query_num in range(NUM_QUERIES):
             for comb in range(NUM_COMBS):
-                curr_bytes = struct.pack('q', -1)
+                curr_bytes = struct.pack('q', long(-1))
                 tpch_sizes.write(curr_bytes)
         for query_num in range(1,23,1):
-            print query_num
             if query_num in exclude:
                 continue
             with open('tpch-cards/{}.csv'.format(query_num), 'r') as card_file:
-                print table_map[query_num]
                 lines = card_file.read().split("\n")[1:]
                 for line in lines:
                     tables = line.split(',')[0].split("-")
@@ -39,34 +37,11 @@ def main():
                     card = long(line.split(',')[1])
                     comb = 0
                     for table in tables:
-                        comb |= table_map[query_num][table]
-                    tpch_sizes.seek((query_num*NUM_COMBS+comb)*8, 0)
-                    struct.pack('q', card)
-
-
-
-        """
-    with open('tpch_sizes', 'wb') as fout:
-        for query_num in range(NUM_QUERIES):
-            for comb in range(NUM_COMBS):
-                curr_bytes = struct.pack('q', -1)
-                fout.write(curr_bytes)
-        print "done creating file"
-        with open('job_sizes', 'rb') as fin:
-            for query_num in range(NUM_QUERIES):
-                print query_num
-                for comb in range(NUM_COMBS):
-                    fin.seek((query_num*NUM_COMBS+comb)*8, 0)
-                    fout.seek((query_num*NUM_COMBS+comb)*8, 0)
-                    num = struct.unpack('q', fin.read(8))[0]
-                    if num >= 0:
-                        count += 1
-                        currquery = get_query(file_contents[query_num], comb)
-                        cur.execute(currquery)
-                        num = int(round(cur.fetchone()[8]))
-                    fout.write(struct.pack('q', num))
-        """
-
+                        comb = comb | 1<<(table_map[query_num][table])
+                    if card == 38760:
+                        print card, bin(comb).count("1"), comb 
+                    tpch_sizes.seek((query_num*NUM_COMBS+comb)*LONG_SIZE, 0)
+                    tpch_sizes.write(struct.pack('q', card))
     print "Total Count: ", count
 
 main()
